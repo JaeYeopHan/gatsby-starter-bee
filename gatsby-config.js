d1c480bdf5bc59aaf6ffeb32ac7d5ce37cf88828
JaeYeopHan <ljyhanll@gmail.com>
2019-02-07 15:44:28 +0900

    :recycle: env: Add emoji plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,7 @@ module.exports = {
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
           `gatsby-remark-autolink-headers`,
+          `gatsby-remark-emoji`,
         ],
       },
     },
