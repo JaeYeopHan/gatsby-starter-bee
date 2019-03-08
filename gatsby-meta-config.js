3fec41012e0d937525329b24ee4d894be24e5c74
jayden-lee <gwanggi.lee@gmail.com>
2019-03-08 13:54:50 +0900

    Add siteUrl

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `Blog posted about ...`,
   author: `[Your Name]`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://www.example.com`, // Your blog site url
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: ``, // Your GitHub account
