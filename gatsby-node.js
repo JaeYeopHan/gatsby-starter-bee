750b2fe57205a305b8d7c35cb8aef763e47bb10d
JaeYeopHan <ljyhanll@gmail.com>
2019-10-12 14:30:13 +0900

    fix: Remove useless code

--- gatsby-node.js
+++ gatsby-node.js
@@ -60,8 +60,6 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
 
-    console.log(value)
-
     createNodeField({
       name: `slug`,
       node,
