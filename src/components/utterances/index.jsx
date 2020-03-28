fb1fb2e40598f45d594144c048ada86820ca9514
JaeYeopHan <ljyhanll@gmail.com>
2020-03-28 14:33:55 +0900

    feat: Add utterences dark theme

--- src/components/utterances/index.jsx
+++ src/components/utterances/index.jsx
@@ -1,17 +1,25 @@
 import React, { useEffect } from 'react'
 
+import * as Dom from '../../utils/dom'
+import { THEME } from '../../constants'
+
 const src = 'https://utteranc.es/client.js'
 const branch = 'master'
+const DARK_THEME = 'photon-dark'
+const LIGHT_THEME = 'github-light'
 
 export const Utterences = ({ repo }) => {
   const rootElm = React.createRef()
 
   useEffect(() => {
+    const isDarkTheme = Dom.hasClassOfBody(THEME.DARK)
     const utterances = document.createElement('script')
     const utterancesConfig = {
       src,
       repo,
       branch,
+      theme: isDarkTheme ? DARK_THEME : LIGHT_THEME,
+      label: 'comment',
       async: true,
       'issue-term': 'pathname',
       crossorigin: 'anonymous',
