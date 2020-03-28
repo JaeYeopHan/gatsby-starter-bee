914a7611f21aed03f83902e3c27f3c7b96a4444b
JaeYeopHan <ljyhanll@gmail.com>
2020-03-28 15:02:37 +0900

    fix: Change maxWidth option of gatsby-remark-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
+              maxWidth: 1200,
               linkImagesToOriginal: false,
             },
           },
