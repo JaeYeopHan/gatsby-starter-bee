5a258121e14b53ca6318046a2071bb99a8d6b008
JaeYeopHan <ljyhanll@gmail.com>
2020-06-14 17:43:05 +0900

    fix: Extract to useIntersectionObserver hooks

---
+++ src/hooks/useIntersectionObserver.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+import * as IOManager from '../utils/visible'
+
+export function useIntersectionObserver() {
+    useEffect(() => {
+        IOManager.init()
+        return () => {
+            IOManager.destroy()
+        }
+    }, [])
+
+    useEffect(() => {
+        IOManager.refreshObserver()
+    })
+}
+
