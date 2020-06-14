d7f42029cc7e5ebde612d1a30978136b4f070be9
JaeYeopHan <ljyhanll@gmail.com>
2020-06-14 18:03:04 +0900

    chore: Polish

--- src/hooks/useRenderedCount.js
+++ src/hooks/useRenderedCount.js
@@ -2,16 +2,15 @@ import { useState, useEffect, useRef } from 'react'
 import * as Storage from '../utils/storage'
 
 export function useRenderedCount() {
-    const initialCount = Storage.getCount(1)
-    const [count, setCount] = useState(initialCount)
-    const countRef = useRef(count)
-    const increaseCount = () => setCount(prev => prev + 1)
+  const initialCount = Storage.getCount(1)
+  const [count, setCount] = useState(initialCount)
+  const countRef = useRef(count)
+  const increaseCount = () => setCount(prev => prev + 1)
 
-    useEffect(() => {
-        countRef.current = count
-        Storage.setCount(count)
-    }, [count])
+  useEffect(() => {
+    countRef.current = count
+    Storage.setCount(count)
+  }, [count])
 
-    return [count, countRef, increaseCount]
+  return [count, countRef, increaseCount]
 }
-
