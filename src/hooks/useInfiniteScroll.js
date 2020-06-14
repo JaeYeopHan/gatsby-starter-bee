caf41da8a13f63f406548d6dbc38d2e946b806a3
JaeYeopHan <ljyhanll@gmail.com>
2020-06-14 19:30:18 +0900

    fix: Change hooks

--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -5,25 +5,24 @@ import * as EventManager from '../utils/event-manager'
 const BASE_LINE = 80
 
 function getDistance(currentPos) {
-    return Dom.getDocumentHeight() - currentPos
+  return Dom.getDocumentHeight() - currentPos
 }
 
 export function useInfiniteScroll(callback, condition) {
-    const onScroll = useCallback(() => {
-        const currentPos = window.scrollY + window.innerHeight
-        const isTriggerPos = () => getDistance(currentPos) < BASE_LINE
+  const onScroll = useCallback(() => {
+    const currentPos = window.scrollY + window.innerHeight
+    const isTriggerPos = () => getDistance(currentPos) < BASE_LINE
 
-        return EventManager.toFit(callback, {
-            dismissCondition: !isTriggerPos(),
-            triggerCondition: isTriggerPos() && condition(),
-        })()
-    }, [condition])
+    return EventManager.toFit(callback, {
+      dismissCondition: !isTriggerPos(),
+      triggerCondition: isTriggerPos() && condition(),
+    })()
+  }, [condition])
 
-    useEffect(() => {
-        window.addEventListener(`scroll`, onScroll, { passive: false })
-        return () => {
-            window.removeEventListener(`scroll`, onScroll, { passive: false })
-        }
-    }, [])
+  useEffect(() => {
+    window.addEventListener(`scroll`, onScroll, { passive: false })
+    return () => {
+      window.removeEventListener(`scroll`, onScroll, { passive: false })
+    }
+  }, [])
 }
-
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { graphql } from 'gatsby'
 import _ from 'lodash'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Bio } from '../components/bio'
 import { Category } from '../components/category'
 import { Contents } from '../components/contents'
@@ -9,19 +9,40 @@ import { HOME_TITLE } from '../constants'
 import { useCategory } from '../hooks/useCategory'
 import { useIntersectionObserver } from '../hooks/useIntersectionObserver'
 import { useRenderedCount } from '../hooks/useRenderedCount'
-import { useInfiniteScroll } from '../hooks/useInfiniteScroll'
+import { useScrollEvent } from '../hooks/useScrollEvent'
 import { Layout } from '../layout'
+import * as Dom from '../utils/dom'
+import * as EventManager from '../utils/event-manager'
+
+const BASE_LINE = 80
+
+function getDistance(currentPos) {
+  return Dom.getDocumentHeight() - currentPos
+}
 
 export default ({ data, location }) => {
   const { siteMetadata } = data.site
   const { countOfInitialPost } = siteMetadata.configs
   const posts = data.allMarkdownRemark.edges
-  const categories = _.uniq(posts.map(({ node }) => node.frontmatter.category))
+  const categories = useMemo(
+    () => _.uniq(posts.map(({ node }) => node.frontmatter.category)),
+    []
+  )
   const [count, countRef, increaseCount] = useRenderedCount()
   const [category, selectCategory] = useCategory()
 
   useIntersectionObserver()
-  useInfiniteScroll(increaseCount, () => posts.length > countRef.current * countOfInitialPost)
+  useScrollEvent(() => {
+    const currentPos = window.scrollY + window.innerHeight
+    const isTriggerPos = () => getDistance(currentPos) < BASE_LINE
+    const doesNeedMore = () =>
+      posts.length > countRef.current * countOfInitialPost
+
+    return EventManager.toFit(increaseCount, {
+      dismissCondition: () => !isTriggerPos(),
+      triggerCondition: () => isTriggerPos() && doesNeedMore(),
+    })()
+  })
 
   return (
     <Layout location={location} title={siteMetadata.title}>
