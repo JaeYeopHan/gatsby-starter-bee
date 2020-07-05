c05ea4b06664acefd74be0bd240d800e8b153821
JaeYeopHan <ljyhanll@gmail.com>
2020-07-05 12:48:03 +0900

    feat: add smooth scroll to category component

--- src/components/category/index.jsx
+++ src/components/category/index.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-
-import { Item } from './item'
+import React, { useCallback, useRef } from 'react'
 import { rhythm } from '../../utils/typography'
-
 import './index.scss'
+import { Item } from './item'
 
 export const Category = ({ categories, category, selectCategory }) => {
+  const containerRef = useRef(null)
+
+  const scrollToCenter = useCallback(tabRef => {
+    const { offsetWidth: tabWidth } = tabRef.current
+    const { scrollLeft, offsetWidth: containerWidth } = containerRef.current
+    const tabLeft = tabRef.current.getBoundingClientRect().left
+    const containerLeft = containerRef.current.getBoundingClientRect().left
+    const refineLeft = tabLeft - containerLeft
+    const targetScollX = scrollLeft + refineLeft - (containerWidth / 2) + (tabWidth / 2)
+
+    containerRef.current.scrollTo(targetScollX, 0)
+  }, [containerRef])
+
   return (
     <ul
+      ref={containerRef}
       className="category-container"
       role="tablist"
       id="category"
@@ -15,13 +27,14 @@ export const Category = ({ categories, category, selectCategory }) => {
         margin: `0 -${rhythm(3 / 4)}`,
       }}
     >
-      <Item title={'All'} category={category} selectCategory={selectCategory} />
-      {categories.map((item, idx) => (
+      <Item title={'All'} selectedCategory={category} onClick={selectCategory} scrollToCenter={scrollToCenter} />
+      {categories.map((title, idx) => (
         <Item
           key={idx}
-          title={item}
-          category={category}
-          selectCategory={selectCategory}
+          title={title}
+          selectedCategory={category}
+          onClick={selectCategory}
+          scrollToCenter={scrollToCenter}
         />
       ))}
     </ul>
--- src/components/category/item/index.jsx
+++ src/components/category/item/index.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useRef, useCallback, useEffect } from 'react'
 
-export const Item = ({ title, category, selectCategory }) => (
-  <li
-    className="item"
-    role="tab"
-    aria-selected={category === title ? 'true' : 'false'}
-  >
-    <div onClick={() => selectCategory(title)}>{title}</div>
-  </li>
-)
+export const Item = ({ title, selectedCategory, onClick, scrollToCenter }) => {
+  const tabRef = useRef(null)
+
+  const handleClick = useCallback(() => {
+    scrollToCenter(tabRef)
+    onClick(title)
+  }, [tabRef])
+
+  useEffect(() => {
+    if (selectedCategory === title) {
+      scrollToCenter(tabRef)
+    }
+  }, [selectedCategory, tabRef])
+
+  return (
+    <li
+      ref={tabRef}
+      className="item"
+      role="tab"
+      aria-selected={selectedCategory === title ? 'true' : 'false'}
+    >
+      <div onClick={handleClick}>{title}</div>
+    </li>
+  )
+}
