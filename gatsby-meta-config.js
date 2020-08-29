23c9e9d1c2ecd380ae6980e2f2381e16de34e875
정훈 송 <tghyyhjuujki@naver.com>
2020-08-29 19:54:44 +0900

    update

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -26,5 +26,5 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'UA-176748195-1', // Add your google analytics tranking ID
 }
