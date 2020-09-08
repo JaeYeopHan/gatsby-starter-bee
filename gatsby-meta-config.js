6469dfb24b8356689210c852ba2d82d54b05d27b
김선도 <16316626+sundoforce@users.noreply.github.com>
2020-09-09 00:40:24 +0900

    Update gatsby-meta-config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: `Bee starter`,
+  title: `sundo.kim`,
   description: `Blog posted about ...`,
   author: `[Your Name]`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
@@ -15,13 +15,13 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'sundoforce/gatsby-starter-bee', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'sundo',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
