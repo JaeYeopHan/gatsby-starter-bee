08ff6d7f4c6e9d4144f649ed43b6c3c96592ff50
sdk <admin@sundo.kim>
2021-05-09 10:55:20 +0900

    config rollback

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: `sundo.kim`,
+  title: `Bee starter`,
   description: `Blog posted about ...`,
   author: `[Your Name]`,
   introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
@@ -15,18 +15,18 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'sundoforce/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'sundo',
+    buyMeACoffeeId: 'jbee',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
   ga: '', // Add your google analytics tranking ID
-  ad: '0', // Add your google adSense  ID
+  ad: '0', // Add your google andsend  ID
 
 }
